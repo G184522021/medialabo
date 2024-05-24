232ea46b2c55722f249cf7b25c61ab5cd46355c8
G184522021 <218452@st.takushoku-u.ac.jp>
2024-05-24 15:06:18 +0900

    kazuate.jsを新規作成

--- kazuate.js
+++ kazuate.js
@@ -22,22 +22,52 @@ function hantei() {
   // kotae と yoso が一致するかどうか調べて結果を出力
   // 課題3-1における出力先はコンソール
 kaisu = kaisu+1;
+
+let kaisuSpan = document.getElementById('kaisu');
+    let answerSpan = document.getElementById('answer');
+    let resultP = document.getElementById('result');
+    let p = document.createElement("p");
 if (kaisu >=4) {
+    kaisuSpan.textContent = kaisu;
+answerSpan.textContent = yoso;
     console.log(kaisu+"回目の予想: "+ yoso);
-    console.log("答えはでした．すでにゲームは終わっています");
+    console.log("答えは"+kotae+"でした．すでにゲームは終わっています");
+    resultP.textContent = kaisuSpan + "回目の予想： "+ answerSpan;
+    resultP.textContent = "答えは"+kotae+"でした．すでにゲームは終わっています";
+    p.insertAdjacentElement('afterend' ,resultP);
 } else if (kaisu<= 2 && yoso < kotae) {
+    kaisuSpan.textContent = kaisu;
+answerSpan.textContent = yoso;
     console.log(kaisu+"回目の予想: "+ yoso);
     console.log("まちがい.答えはもっと大きいですよ");
+    resultP.textContent = kaisuSpan + "回目の予想： "+ answerSpan;
+    resultP.textContent = "まちがい.答えはもっと大きいですよ";
+    p.insertAdjacentElement('afterend' ,resultP);
 } else if (kaisu<= 2 && yoso > kotae) {
+    kaisuSpan.textContent = kaisu;
+answerSpan.textContent = yoso;
     console.log(kaisu+"回目の予想: "+ yoso);
     console.log("まちがい.答えはもっと小さいですよ")
+    resultP.textContent = kaisuSpan + "回目の予想： "+ answerSpan;
+    resultP.textContent = "まちがい.答えはもっと小さいですよ";
+    p.insertAdjacentElement('afterend' ,resultP);
 } else if (yoso === kotae){
+    kaisuSpan.textContent = kaisu;
+answerSpan.textContent = yoso;
     console.log(kaisu+"回目の予想: "+ yoso);
     console.log("正解です.おめでとう");
     kaisu= kaisu + 3;
+    resultP.textContent = kaisuSpan + "回目の予想： "+ answerSpan;
+    resultP.textContent = "正解です.おめでとう";
+    p.insertAdjacentElement('afterend' ,resultP);
 }  else if (kaisu === 3){
+    kaisuSpan.textContent = kaisu;
+answerSpan.textContent = yoso;
     console.log(kaisu+"回目の予想: "+ yoso);
     console.log("まちがい.残念でした答えは"+kotae+"です");
+    resultP.textContent = kaisuSpan + "回目の予想： "+ answerSpan;
+    resultP.textContent = "まちがい.残念でした答えは"+kotae+"です";
+    p.insertAdjacentElement('afterend' ,resultP);
 }
 
 }
